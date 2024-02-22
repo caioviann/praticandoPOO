36bb8bb7a58d58d3679ea2ec57866363a695c9c1
Caio Henrique <caiohenriqueviana01@gmail.com>
2024-02-22 13:43:04 -0300

    first ccommit

---
+++ src/henrique/caio/minhasmusicas/principal/Principal.java
@@ -0,0 +1,37 @@
+package henrique.caio.minhasmusicas.principal;
+
+import henrique.caio.minhasmusicas.modelos.MinhasPreferidas;
+import henrique.caio.minhasmusicas.modelos.Musica;
+import henrique.caio.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
